aaabd73e99df784e501da5a2f4f977f166dc44c8
Chef Software <chef@chef.io>
2015-12-28 20:57:03 +0000

    move to attributes

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['web']['width'] = '1280'
+default['web']['height'] = '768'
+default['web']['base'] = 'http://external.kongregate-games.com/gamez/0011/5608/live/'
+default['web']['src'] = 'http://external.kongregate-games.com/gamez/0011/5608/live/embeddable_115608.swf'
